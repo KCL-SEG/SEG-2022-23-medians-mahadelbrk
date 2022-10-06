0e2eb52cfa1847eb22dab1d00d3ce2732174c488
mahadelbrk <maha.albarrak@kcl.ac.uk>
2022-10-06 18:38:26 +0100

    Hello this is working

--- median.py
+++ median.py
@@ -3,7 +3,8 @@
 
 while True:
     try:
-        print("Enter a list of numbers separated by commas: ")
+        print("Enter a list of numbers separated by commas:  git ")
+         print("Enter a list of numbers separated by commas:  git ")
         numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input-2 could not be converted to a number!")
